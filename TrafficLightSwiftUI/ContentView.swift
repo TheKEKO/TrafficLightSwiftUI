ecf45a735aec817343d76717f2c171b870c966de
Aleksandr F <a7214370@gmail.com>
2022-05-12 17:54:16 +0300

    Setup View

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -14,13 +14,27 @@ struct ContentView: View {
                 .ignoresSafeArea()
             VStack {
                 ColorCircleView(color: .red
-                    .opacity(0.55))
+                    .opacity(0.40))
+                .padding(.bottom, 10.0)
                 ColorCircleView(color: .yellow
-                    .opacity(0.55))
+                    .opacity(0.40))
+                .padding(.bottom, 10.0)
                 ColorCircleView(color: .green
-                    .opacity(0.55))
+                    .opacity(0.40))
+                .padding(.bottom, 10.0)
+                Spacer()
+                Button(action: {}) {
+                    Text("START")
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .frame(width: 160, height: 50)
+                        .background(Color.blue)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 6))
+                        .cornerRadius(10)
+                }
             }
-            .padding()
+            .padding(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
         }
     }
 }
@@ -28,6 +42,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-        
+     
     }
 }
