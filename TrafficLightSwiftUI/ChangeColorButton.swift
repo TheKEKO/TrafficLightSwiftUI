b3984f991815468b38f6c9f32589425df63d2169
Aleksandr F <a7214370@gmail.com>
2022-05-15 20:24:47 +0300

    Task completed final

---
+++ TrafficLightSwiftUI/ChangeColorButton.swift
@@ -0,0 +1,20 @@
+//
+//  ChangeColorButton.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Aleksandr F. on 15.05.2022.
+//
+
+import SwiftUI
+
+struct ChangeColorButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChangeColorButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangeColorButton()
+    }
+}
